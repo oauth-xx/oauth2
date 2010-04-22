311d9f42e52b832119170d90e818f0f0b0078851
Michael Bleigh <michael@intridea.com>
2010-04-21 20:22:04 -0400

    Initial commit to oauth2.

---
+++ spec/oauth2_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Oauth2" do
+  it "fails" do
+    fail "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
