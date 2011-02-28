fa4fcba0aee124bdde7c423ce57150ef3287da69
Arjun Anand <arjun.anand84@gmail.com>
2011-02-28 15:23:10 -0500

    Facebook sets the "expires" param that the current gem doesnt support
    refer: http://developers.facebook.com/docs/authentication/
    
    specifically: "In addition to the access token (the access_token parameter), the response contains the number of seconds until the token expires (the expires parameter)"

--- spec/oauth2/strategy/web_server_spec.rb
+++ spec/oauth2/strategy/web_server_spec.rb
@@ -11,6 +11,8 @@ describe OAuth2::Strategy::WebServer do
               [200, {}, 'expires_in=600&access_token=salmon&refresh_token=trout&extra_param=steve']
             when "json"
               [200, {}, '{"expires_in":600,"access_token":"salmon","refresh_token":"trout","extra_param":"steve"}']
+            when "from_facebook"
+              [200, {}, 'expires=600&access_token=salmon&refresh_token=trout&extra_param=steve']
           end
         end
       end
@@ -34,7 +36,7 @@ describe OAuth2::Strategy::WebServer do
     end
   end
 
-  %w(json formencoded).each do |mode|
+  %w(json formencoded from_facebook).each do |mode|
     describe "#get_access_token (#{mode})" do
       before do
         @mode = mode
