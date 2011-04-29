a34f3f6fd0e5d472ec482ea2a19150e6dc6bd616
Paul Walker <github@paulwalker.tv>
2011-04-29 16:21:06 -0700

    cleanup of rdoc param comments, removed json attr_accessor, removed commented out code

--- lib/oauth2/client.rb
+++ lib/oauth2/client.rb
@@ -3,7 +3,6 @@ require 'faraday'
 module OAuth2
   class Client
     attr_accessor :id, :secret, :site, :connection, :options
-    attr_writer :json
 
     # Instantiate a new OAuth 2.0 client using the
     # client ID and client secret registered to your
@@ -12,17 +11,15 @@ module OAuth2
     # Options:
     #
     # <tt>:site</tt> :: Specify a base URL for your OAuth 2.0 client.
-    # <tt>:authorize_path</tt> :: Specify the path to the authorization endpoint.
     # <tt>:authorize_url</tt> :: Specify a full URL of the authorization endpoint.
-    # <tt>:access_token_path</tt> :: Specify the path to the access token endpoint.
+    # <tt>:access_token_url</tt> :: Specify the full URL of the access token endpoint.
     # <tt>:access_token_method</tt> :: Specify the method to use for token endpoints, can be :get or :post
     # (note: for Facebook this should be :get and for Google this should be :post)
-    # <tt>:access_token_url</tt> :: Specify the full URL of the access token endpoint.
     # <tt>:parse_json</tt> :: If true, <tt>application/json</tt> responses will be automatically parsed.
+    # <tt>:raise_errors</tt> :: Default true. When false it will then return the error status and response instead of raising an exception.
     # <tt>:ssl</tt> :: Specify SSL options for the connection.
     # <tt>:adapter</tt> :: The name of the Faraday::Adapter::* class to use, e.g. :net_http. To pass arguments
     # to the adapter pass an array here, e.g. [:action_dispatch, my_test_session]
-    # <tt>:raise_errors</tt> :: Default true. When false it will then return the error status and response instead of raising an exception.
     def initialize(client_id, client_secret, opts={})
       @id = client_id
       @secret = client_secret
@@ -70,12 +67,10 @@ module OAuth2
     end
 
     def authorize_url(params=nil)
-      # path = options[:authorize_url] || options[:authorize_path] || "/oauth/authorize"
       connection.build_url(options[:authorize_url], params).to_s
     end
 
     def access_token_url(params=nil)
-      # path = options[:access_token_url] || options[:access_token_path] || "/oauth/access_token"
       connection.build_url(options[:access_token_url], params).to_s
     end
 
