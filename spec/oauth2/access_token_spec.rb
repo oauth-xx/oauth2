ebe4be038ec14b3496827d29cb224235e1c9f468
Erik Michaels-Ober <sferik@gmail.com>
2011-05-18 15:12:19 -0400

    Fix specs

--- spec/oauth2/access_token_spec.rb
+++ spec/oauth2/access_token_spec.rb
@@ -12,7 +12,7 @@ describe AccessToken do
       builder.adapter :test do |stub|
         VERBS.each do |verb|
           stub.send(verb, '/token/header') { |env| [200, {}, env[:request_headers]['Authorization']] }
-          stub.send(verb, "/token/query?oauth_token=#{token}") { |env| [200, {}, Addressable::URI.parse(env[:url]).query_values['oauth_token']] }
+          stub.send(verb, "/token/query?bearer_token=#{token}") { |env| [200, {}, Addressable::URI.parse(env[:url]).query_values['bearer_token']] }
           stub.send(verb, '/token/body') { |env| [200, {}, env[:body]] }
         end
         stub.post('/oauth/token') { |env| [200, {}, refresh_body]}
@@ -33,26 +33,26 @@ describe AccessToken do
       target.params.should include('foo')
       target.params['foo'].should == 'bar'
     end
-    
+
     def assert_initialized_token(target)
       target.token.should eq(token)
       target.should be_expires
       target.params.keys.should include('foo')
       target.params['foo'].should == 'bar'
     end
-    
+
     it 'initializes with a Hash' do
       hash = { :access_token => token, :expires_at => Time.now.to_i+200, 'foo' => 'bar' }
       target = AccessToken.from_hash(client, hash)
       assert_initialized_token(target)
     end
-    
+
     it 'initalizes with a form-urlencoded key/value string' do
       kvform = "access_token=#{token}&expires_at=#{Time.now.to_i+200}&foo=bar"
       target = AccessToken.from_kvform(client, kvform)
       assert_initialized_token(target)
     end
-    
+
     it 'sets options' do
       target = AccessToken.new(client, token, :param_name => 'foo', :header_format => 'Bearer %', :mode => :body)
       target.options[:param_name].should == 'foo'
@@ -60,37 +60,37 @@ describe AccessToken do
       target.options[:mode].should == :body
     end
   end
-  
+
   describe '#request' do
     context ':mode => :header' do
-      before :all do 
+      before :all do
         subject.options[:mode] = :header
       end
-      
+
       VERBS.each do |verb|
         it "sends the token in the Authorization header for a #{verb.to_s.upcase} request" do
           subject.post('/token/header').body.should include(token)
         end
       end
     end
-    
+
     context ':mode => :query' do
       before :all do
         subject.options[:mode] = :query
       end
-      
+
       VERBS.each do |verb|
         it "sends the token in the Authorization header for a #{verb.to_s.upcase} request" do
           subject.post('/token/query').body.should == token
         end
       end
     end
-    
+
     context ':mode => :body' do
       before :all do
         subject.options[:mode] = :body
       end
-      
+
       VERBS.each do |verb|
         it "sends the token in the Authorization header for a #{verb.to_s.upcase} request" do
           subject.post('/token/body').body.split('=').last.should == token
@@ -128,13 +128,13 @@ describe AccessToken do
       Time.stub!(:now).and_return(@now)
       access.should be_expired
     end
-    
+
   end
 
   describe '#refresh!' do
     it 'returns a refresh token with appropriate values carried over' do
-      access = AccessToken.new(client, token, :refresh_token  => 'abaca', 
-                                              :expires_in     => 600, 
+      access = AccessToken.new(client, token, :refresh_token  => 'abaca',
+                                              :expires_in     => 600,
                                               :param_name     => 'o_param')
       refreshed = access.refresh!
       access.client.should == refreshed.client
