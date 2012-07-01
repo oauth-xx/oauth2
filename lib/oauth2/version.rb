19540a737aab816fe838d150c9a401e694c58aa5
Erik Michaels-Ober <sferik@gmail.com>
2012-06-30 19:22:37 -0600

    Bump version to 0.8.0

--- lib/oauth2/version.rb
+++ lib/oauth2/version.rb
@@ -1,8 +1,8 @@
 module OAuth2
   class Version
     MAJOR = 0 unless defined? MAJOR
-    MINOR = 7 unless defined? MINOR
-    PATCH = 1 unless defined? PATCH
+    MINOR = 8 unless defined? MINOR
+    PATCH = 0 unless defined? PATCH
     PRE = nil unless defined? PRE
 
     class << self
