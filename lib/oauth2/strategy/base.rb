ebeea112030975f7748f8d622f1696380f97c9e2
Ellis Berner <eberner@gmail.com>
2014-09-12 13:10:49 -0700

    auth_code: default to header based authorization

--- lib/oauth2/strategy/base.rb
+++ lib/oauth2/strategy/base.rb
@@ -11,6 +11,14 @@ module OAuth2
       def client_params
         {'client_id' => @client.id, 'client_secret' => @client.secret}
       end
+
+      # Returns the Authorization header value for Basic Authentication
+      #
+      # @param [String] The client ID
+      # @param [String] the client secret
+      def authorization(client_id, client_secret)
+        'Basic ' + Base64.encode64(client_id + ':' + client_secret).gsub("\n", '')
+      end
     end
   end
 end
