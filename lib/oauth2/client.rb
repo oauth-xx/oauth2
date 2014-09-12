148747335fff0d0d1eb8cde8b536e1c3d062962b
Ellis Berner <eberner@gmail.com>
2014-09-12 14:24:03 -0700

    spec: add specs for auth_scheme

--- lib/oauth2/client.rb
+++ lib/oauth2/client.rb
@@ -127,14 +127,15 @@ module OAuth2
     # @return [AccessToken] the initalized AccessToken
     def get_token(params, access_token_opts = {}, access_token_class = AccessToken) # rubocop:disable Metrics/AbcSize
       opts = {:raise_errors => options[:raise_errors], :parse => params.delete(:parse)}
+      headers = params.delete(:headers)
       if options[:token_method] == :post
-        headers = params.delete(:headers)
         opts[:body] = params
         opts[:headers] = {'Content-Type' => 'application/x-www-form-urlencoded'}
-        opts[:headers].merge!(headers) if headers
       else
         opts[:params] = params
+        opts[:headers] = {}
       end
+      opts[:headers].merge!(headers) if headers
       response = request(options[:token_method], token_url, opts)
       error = Error.new(response)
       raise(error) if options[:raise_errors] && !(response.parsed.is_a?(Hash) && response.parsed['access_token'])
