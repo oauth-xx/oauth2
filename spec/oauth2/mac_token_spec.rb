093ba5a0be58894132c45c935556f93d36d0d585
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-09-16 11:01:26 -0500

    Update MAC to draft 05

--- spec/oauth2/mac_token_spec.rb
+++ spec/oauth2/mac_token_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe MACToken do
-  subject { described_class.new(client, token, 'abc123') }
+  subject { described_class.new(client, token, 'abc123', kid: kid) }
 
+  let(:kid) { 'this-token' }
   let(:token) { 'monkey' }
   let(:client) do
     Client.new('abc', 'def', :site => 'https://api.example.com') do |builder|
@@ -44,8 +45,8 @@ RSpec.describe MACToken do
 
   describe '#request' do
     VERBS.each do |verb|
-      it "sends the token in the Authorization header for a #{verb.to_s.upcase} request" do
-        expect(subject.post('/token/header').body).to include("MAC id=\"#{token}\"")
+      it "sends the kid in the Authorization header for a #{verb.to_s.upcase} request" do
+        expect(subject.post('/token/header').body).to include("MAC kid=\"#{kid}\"")
       end
     end
   end
@@ -60,7 +61,7 @@ RSpec.describe MACToken do
 
     it 'generates the proper format' do
       header = subject.header('get', 'https://www.example.com/hello?a=1')
-      expect(header).to match(/MAC id="#{token}", ts="[0-9]+", nonce="[^"]+", mac="[^"]+"/)
+      expect(header).to match(/MAC kid="#{kid}", ts="[0-9]+", seq-nr="[0-9]+", mac="[^"]+"/)
     end
 
     it 'passes ArgumentError with an invalid url' do
@@ -68,14 +69,14 @@ RSpec.describe MACToken do
     end
 
     it 'passes URI::InvalidURIError through' do
-      expect { subject.header('get', nil) }.to raise_error(URI::InvalidURIError)
+      expect { subject.header('get', '\\') }.to raise_error(URI::InvalidURIError)
     end
   end
 
   describe '#signature' do
     it 'generates properly' do
-      signature = subject.signature(0, 'random-string', 'get', URI('https://www.google.com'))
-      expect(signature).to eq('rMDjVA3VJj3v1OmxM29QQljKia6msl5rjN83x3bZmi8=')
+      signature = subject.signature(0, 'random-string', 'get', URI('https://www.google.com/hai?this=1'))
+      expect(signature).to eq('13wqHvYkM7uDKpT9Yc8HnDLxa+4yjv2G1qV1kkqpbdc=')
     end
   end
 
