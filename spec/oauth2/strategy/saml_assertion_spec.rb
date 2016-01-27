118192589ee1c6f907c936f5c4faf55752364383
Jeffery Yeary <jeff@debug.ninja>
2016-01-27 16:25:34 -0500

    slim this down

--- spec/oauth2/strategy/saml_assertion_spec.rb
+++ spec/oauth2/strategy/saml_assertion_spec.rb
@@ -1,32 +1,12 @@
 require 'helper'
 
 describe OAuth2::Strategy::SamlAssertion do
-  #let(:client) { double('client', id: '123', secret: 'def') }
-  let(:client) do
-    cli = OAuth2::Client.new('123', 'def')
-    cli.connection.build do |b|
-      b.adapter :test do |stub|
-        stub.post('/oauth/token') do |_env|
-          case @mode
-            when 'formencoded'
-              [200, { 'Content-Type' => 'application/x-www-form-urlencoded' }, 'expires_in=600&access_token=salmon&refresh_token=trout']
-            when 'json'
-              [200, { 'Content-Type' => 'application/json' }, '{"expires_in":600,"access_token":"salmon","refresh_token":"trout"}']
-          end
-        end
-      end
-    end
-    cli
-  end
-
-  subject { client.saml_assertion }
-
-
+  let(:client) { OAuth2::Client.new('123', 'def') }
   let(:token_value) { 'token123' }
   let(:assertion) { 'assertion' }
 
   it 'should throw an exception when checking authorize_url' do
-    expect{subject.authorize_url}.to raise_error(NotImplementedError)
+    expect{client.saml_assertion.authorize_url}.to raise_error(NotImplementedError)
   end
 
   it 'should merge params with client params' do
