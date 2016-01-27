3773e0942ce514e8a92c157480e6f7d9aafe0d28
Jeffery Yeary <jeff@debug.ninja>
2016-01-27 16:44:53 -0500

    matching the format of other strategies

--- lib/oauth2/strategy/saml_assertion.rb
+++ lib/oauth2/strategy/saml_assertion.rb
@@ -1,10 +1,31 @@
 module OAuth2
   module Strategy
+    # The SAML Assertion Strategy
+    #
+    # @see https://tools.ietf.org/html/draft-ietf-oauth-saml2-bearer-23
+    #
+    # Sample usage:
+    #   client = OAuth2::Client.new(client_id, client_secret,
+    #                               :token_url => 'http://api.example.com/token')
+    #
+    #   params = {:grant_type  => 'urn:ietf:params:oauth:grant-type:saml2-bearer',
+    #             :scope => 'scope1,scope2'}
+    #   access = client.saml_assertion.get_token(params, 'auth_scheme' => 'request_body')
+    #   access.token                 # actual access_token string
+    #   access.get("/api/stuff")     # making api calls with access token in header
+    #
     class SamlAssertion < Base
+      # Not used for this strategy
+      #
+      # @raise [NotImplementedError]
       def authorize_url
         fail NotImplementedError, 'The authorization endpoint is not used in this strategy'
       end
 
+      # Retrieve an access token given the specified client.
+      #
+      # @param [Hash] params additional params
+      # @param [Hash] opts options
       def get_token(params = {}, opts = {})
         hash = params.merge(client_params)
         @client.get_token(hash, opts.merge('refresh_token' => nil))
--- oauth2.gemspec
+++ oauth2.gemspec
@@ -10,7 +10,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'multi_xml', '~> 0.5'
   spec.add_dependency 'rack', '~> 1.2'
   spec.add_development_dependency 'bundler', '~> 1.0'
-  spec.authors       = ['Michael Bleigh', 'Erik Michaels-Ober']
+  spec.authors       = ['Michael Bleigh', 'Erik Michaels-Ober', 'Jeffery Yeary']
   spec.description   = 'A Ruby wrapper for the OAuth 2.0 protocol built with a similar style to the original OAuth spec.'
   spec.email         = ['michael@intridea.com', 'sferik@gmail.com']
   spec.files         = %w(.document CONTRIBUTING.md LICENSE.md README.md oauth2.gemspec) + Dir['lib/**/*.rb']
