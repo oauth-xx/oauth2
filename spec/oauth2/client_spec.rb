7d192606189c6d656552af0d2c5d6f214984c1eb
Samuel Cochran <sj26@sj26.com>
2017-10-27 17:22:38 +1100

    More resilient and targetting ENV stubbing
    This breaks when unrelated code changes lead to unexpected ENV access:
    
      1) OAuth2::Client#request outputs to $stdout when OAUTH_DEBUG=true
         Failure/Error: conn = Faraday.new(site, options[:connection_opts])
    
           {"BUNDLER_ORIG_PATH"=>"...", "GREP_COLOR"=>"...", ...} received :[] with unexpected arguments
             expected: ("OAUTH_DEBUG")
                  got: ("https_proxy")
            Please stub a default value first if message might be received with other args as well.
         # ./lib/oauth2/client.rb:57:in `connection'
         # ./lib/oauth2/client.rb:95:in `request'
         # ./spec/oauth2/client_spec.rb:178:in `block (4 levels) in <top (required)>'
         # ./spec/helper.rb:33:in `capture_output'
         # ./spec/oauth2/client_spec.rb:177:in `block (3 levels) in <top (required)>'
    
    Instead we can stub the specific value we care about and make sure we
    put it back when we're done.
    
    This shouldn't be any more global than stubbing ENV as a whole.

--- spec/oauth2/client_spec.rb
+++ spec/oauth2/client_spec.rb
@@ -171,14 +171,29 @@ describe OAuth2::Client do
       expect(response.headers).to eq('Content-Type' => 'text/awesome')
     end
 
-    it 'outputs to $stdout when OAUTH_DEBUG=true' do
-      allow(ENV).to receive(:[]).with('http_proxy').and_return(nil)
-      allow(ENV).to receive(:[]).with('OAUTH_DEBUG').and_return('true')
-      output = capture_output do
-        subject.request(:get, '/success')
+    context 'when OAUTH_DEBUG=true' do
+      around do |example|
+        begin
+          original = ENV['OAUTH_DEBUG']
+          ENV['OAUTH_DEBUG'] = 'true'
+
+          example.call
+        ensure
+          if original.nil?
+            ENV.delete('OAUTH_DEBUG')
+          else
+            ENV['OAUTH_DEBUG'] = original
+          end
+        end
       end
 
-      expect(output).to include 'INFO -- : get https://api.example.com/success', 'INFO -- : get https://api.example.com/success'
+      it 'outputs to $stdout when OAUTH_DEBUG=true' do
+        output = capture_output do
+          subject.request(:get, '/success')
+        end
+
+        expect(output).to include 'INFO -- : get https://api.example.com/success', 'INFO -- : get https://api.example.com/success'
+      end
     end
 
     it 'posts a body' do
