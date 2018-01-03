83a0669320bcaf703c7e7e4830661d38a3473944
Samuel Cochran <sj26@sj26.com>
2018-01-03 16:28:55 +1100

    Disable silly rubocop rule

--- lib/oauth2/access_token.rb
+++ lib/oauth2/access_token.rb
@@ -49,7 +49,7 @@ module OAuth2
       @expires_at &&= @expires_at.to_i
       @expires_at ||= Time.now.to_i + @expires_in if @expires_in
       @options = {:mode          => opts.delete(:mode) || :header,
-                  :header_format => opts.delete(:header_format) || 'Bearer %s',
+                  :header_format => opts.delete(:header_format) || 'Bearer %s', # rubocop:disable FormatStringToken
                   :param_name    => opts.delete(:param_name) || 'access_token'}
       @params = opts
     end
