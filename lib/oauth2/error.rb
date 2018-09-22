9893fa60b7c5cd7d80bb974c239bef15837b25ff
Jeff Moore <pair+jeff@mavenlink.com>
2018-09-21 17:13:45 -0700

    Remove OAuth2::Error having an OAuth2::Response as an attribute
    This was causing various errors when trying to jsonify these objects,
    because OAuth2::Response also has its OAuth2::Error as an attribute.
    E.g. response.error == error.response.  Given the choice between the
    two, being able to access error.response seems more useful, given that
    the Error class has a representation of the response as 'message' on
    itself already.

--- lib/oauth2/error.rb
+++ lib/oauth2/error.rb
@@ -1,12 +1,11 @@
 module OAuth2
   class Error < StandardError
-    attr_reader :response, :code, :description
+    attr_reader :code, :description
 
     # standard error codes include:
     # 'invalid_request', 'invalid_client', 'invalid_token', 'invalid_grant', 'unsupported_grant_type', 'invalid_scope'
     def initialize(response)
       response.error = self
-      @response = response
       message_opts = {}
 
       if response.parsed.is_a?(Hash)
