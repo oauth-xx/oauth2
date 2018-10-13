891de14483882049f53adb4ef99e09e3726e1b44
Peter Boling <peter.boling@gmail.com>
2018-10-13 02:20:51 -0700

    Rubocop updates

--- lib/oauth2/version.rb
+++ lib/oauth2/version.rb
@@ -1,6 +1,6 @@
 module OAuth2
   module Version
-    module_function
+  module_function
 
     # The major version
     #
@@ -35,10 +35,10 @@ module OAuth2
     # @return [Hash]
     def to_h
       {
-          :major => major,
-          :minor => minor,
-          :patch => patch,
-          :pre => pre,
+        :major => major,
+        :minor => minor,
+        :patch => patch,
+        :pre => pre,
       }
     end
 
