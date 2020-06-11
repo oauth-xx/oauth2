82f1474aa6feffc91a567282277b9cc9a9c483b4
anvox <thien.an.vo.nguyen@gmail.com>
2020-06-11 21:25:58 +0700

    Remove unused method

--- lib/oauth2/response.rb
+++ lib/oauth2/response.rb
@@ -118,15 +118,6 @@ module OAuth2
 
       @parser ||= @@parsers[@@content_types[content_type]]
     end
-
-    private
-
-    def to_snake_case(string)
-      string.gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-            .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-            .tr("-", "_")
-            .downcase
-    end
   end
 end
 
