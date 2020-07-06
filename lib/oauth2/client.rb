4dac3d284445d614709ae768e77a56c3d8352766
sqiu <sqiu@zuora.com>
2020-07-06 12:12:32 -0400

    add token in the get_token method

--- lib/oauth2/client.rb
+++ lib/oauth2/client.rb
@@ -159,7 +159,7 @@ module OAuth2
       opts[:headers].merge!(headers)
       response = request(options[:token_method], token_url, opts)
       response_contains_token = response.parsed.is_a?(Hash) &&
-                                (response.parsed['access_token'] || response.parsed['id_token'])
+                                (response.parsed['access_token'] || response.parsed['id_token'] || response.parsed['token'])
 
       if options[:raise_errors] && !response_contains_token
         error = Error.new(response)
