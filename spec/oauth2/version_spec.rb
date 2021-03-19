37be42af290c26f765059588b1445cf39addd0e0
Peter Boling <peter.boling@gmail.com>
2021-03-18 21:23:23 -0600

    Version specs

--- spec/oauth2/version_spec.rb
+++ spec/oauth2/version_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe OAuth2::Version do
     expect(described_class).not_to be nil
   end
 
+  it 'can be a string' do
+    expect(described_class.to_s).to be_a(String)
+  end
+
+  it 'allows Constant access' do
+    expect(described_class::VERSION).to be_a(String)
+  end
+
   it 'is greater than 0.1.0' do
     expect(Gem::Version.new(described_class) > Gem::Version.new('0.1.0')).to be(true)
+  end
+
+  it 'is pre-release' do
+    expect(Gem::Version.new(described_class).prerelease?).to be(true)
   end
 end
