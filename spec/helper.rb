7564b818778daebc1be830bddb31edfddffeb520
Peter Boling <peter.boling@gmail.com>
2022-02-16 22:12:55 +0700

    🐛 Proper Ruby version logic
    Signed-off-by: Peter Boling <peter.boling@gmail.com>

--- spec/helper.rb
+++ spec/helper.rb
@@ -6,11 +6,11 @@ require 'rspec/stubbed_env'
 require 'silent_stream'
 
 ruby_version = Gem::Version.new(RUBY_VERSION)
+minimum_version = ->(version) { ruby_version >= Gem::Version.new(version) && RUBY_ENGINE == 'ruby' }
+coverage = minimum_version.call('2.7')
+debug = minimum_version.call('2.5')
 
-# No need to get coverage for older versions of Ruby
-coverable_version = Gem::Version.new('2.7')
-
-if ruby_version >= coverable_version && RUBY_ENGINE == 'ruby'
+if coverage
   require 'simplecov'
   require 'coveralls'
   SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
@@ -29,7 +29,7 @@ require 'addressable/uri'
 Faraday.default_adapter = :test
 
 DEBUG = ENV['DEBUG'] == 'true'
-require 'byebug' if DEBUG && RUBY_VERSION >= '2.6'
+require 'byebug' if DEBUG && debug
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
