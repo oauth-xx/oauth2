cdd6a5f256045d0c9aae0ee445d995b3af8bd307
Peter H. Boling <peter.boling@gmail.com>
2025-05-14 08:28:10 +0700

    👷 Fixes for truffleruby & jruby

--- spec/oauth2/strategy/assertion_spec.rb
+++ spec/oauth2/strategy/assertion_spec.rb
@@ -129,7 +129,7 @@ RSpec.describe OAuth2::Strategy::Assertion do
             # this behavior is handled by the JWT gem, but this should make sure it is consistent
             # On old Ruby (versions 2.4 and below) the error raised was different because
             #   an old version (< v2.4) of the jwt gem gets installed.
-            if Gem::Version.new(JWT::VERSION) >= Gem::Version.new("2.4")
+            if Gem::Version.create(JWT::VERSION::STRING) >= Gem::Version.create("2.4")
               expect { client_assertion.get_token(claims, encoding_opts) }.to raise_error(JWT::EncodeError, "Unsupported signing method")
             else
               expect { client_assertion.get_token(claims, encoding_opts) }.to raise_error(NotImplementedError)
